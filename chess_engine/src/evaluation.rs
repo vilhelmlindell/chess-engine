28331e94e8b1fc2a9fc374ad4f6081fd684b9a0f
Vilhelm Lindell <95926200+vilhelmlindell@users.noreply.github.com>
2023-05-09 22:08:24 +0200

    Delete chess_engine/src directory

--- chess_engine/src/evaluation.rs
+++
@@ -1,7 +0,0 @@
-use crate::board::Board;
-
-impl Board {
-    pub fn evaluate(&self) -> i32 {
-        self.material_balance + self.positional_balance
-    }
-}
--- chess_engine/src/move_ordering.rs
+++
@@ -1,7 +0,0 @@
-//use crate::{board::Board, piece_move::Move};
-//
-//impl Board {
-//    pub fn order_moves(moves: &mut Vec<Move>) -> Vec<Move> {
-//        moves.sort_unstable_by(|a, b| a)
-//    }
-//}
